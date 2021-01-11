9f2d68880e9004ead5d0b37c5dd2ef6587fe3dc1
Shin, Seong-il <callmeshin75@gmail.com>
2021-01-11 14:48:09 +0900

    feat : add conscase upload in admin pages

--- src/components/Questions/QuestionDocument.js
+++ src/components/Questions/QuestionDocument.js
@@ -4,105 +4,109 @@ import CheckPassword from "components/CheckPassword";
 import { fireStoreService } from "fbase";
 
 function QuestionDocument() {
-  const location = useLocation();
-  const question = location.state.question;
-  const history = useHistory();
-  const [isPassword, setIsPassword] = useState(question.password !== "");
-  const [newComment, setNewComment] = useState("");
-  const [comments, setComments] = useState(question.comment);
-  const nextCommentId = useRef(Object.keys(comments).length);
+   const location = useLocation();
+   const question = location.state.question;
+   const history = useHistory();
+   const [isPassword, setIsPassword] = useState(question.password !== "");
+   const [newComment, setNewComment] = useState("");
+   const [comments, setComments] = useState(question.comment);
+   const nextCommentId = useRef(Object.keys(comments).length);
 
-  useEffect(() => {
-    setComments(question.comment);
-  }, [question.comment]);
+   useEffect(() => {
+      setComments(question.comment);
+   }, [question.comment]);
 
-  const onToggleIsPassword = () => {
-    setIsPassword((prev) => !prev);
-  };
+   const onToggleIsPassword = () => {
+      setIsPassword((prev) => !prev);
+   };
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const newCommentObj = {
-      comment: newComment,
-      createdAt: Date.now(),
-      isAdmin: false,
-    };
-    setComments((prev) => ({
-      ...comments,
-      [nextCommentId.current]: newCommentObj,
-    }));
-    const questionObj = {
-      ...question,
-      comment: { ...comments, [nextCommentId.current]: newCommentObj },
-    };
-    await fireStoreService
-      .collection("questions")
-      .doc(question.id)
-      .set(questionObj);
-    setNewComment((prev) => "");
-    nextCommentId.current += 1;
-  };
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === "newComment") {
-      setNewComment((prev) => value);
-    }
-  };
-  return (
-    <>
-      {isPassword ? (
-        <CheckPassword
-          password={question.password}
-          onToggleIsPassword={onToggleIsPassword}
-          history={history}
-        />
-      ) : (
-        <div>
-          <h4>{question.subject}</h4>
-          <div>
-            {Object.values(question.files).map((file, index) => (
-              <a
-                href={file.URL}
-                download={file.name}
-                key={index}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {file.name}
-              </a>
-            ))}
-          </div>
-          <div>{question.text}</div>
-          <div>
-            <button>삭제</button>
-            <button>수정</button>
-          </div>
-          <div>
-            <form onSubmit={onSubmit}>
-              <span>댓글</span>
-              <br></br>
-              <input
-                name="newComment"
-                value={newComment}
-                onChange={onChange}
-                maxLength={1000}
-                style={{ width: 500, height: 80 }}
-              ></input>
-              <br />
-              <input type="submit" value="등록" />
-            </form>
-          </div>
-          {Object.values(comments).map((comment, index) => (
-            <div key={index}>
-              <span>{comment.comment}</span>
-              <span>{comment.createdAt}</span>
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      let newCommentObj = {};
+      if (newComment !== "") {
+         newCommentObj = {
+            comment: newComment,
+            createdAt: Date.now(),
+            isAdmin: false,
+         };
+         setComments((prev) => ({
+            ...comments,
+            [nextCommentId.current]: newCommentObj,
+         }));
+      }
+      const questionObj = {
+         ...question,
+         comment: { ...comments, [nextCommentId.current]: newCommentObj },
+         answered: false,
+      };
+      await fireStoreService
+         .collection("questions")
+         .doc(question.id)
+         .set(questionObj);
+      setNewComment((prev) => "");
+      nextCommentId.current += 1;
+   };
+   const onChange = (event) => {
+      const {
+         target: { name, value },
+      } = event;
+      if (name === "newComment") {
+         setNewComment((prev) => value);
+      }
+   };
+   return (
+      <>
+         {isPassword ? (
+            <CheckPassword
+               password={question.password}
+               onToggleIsPassword={onToggleIsPassword}
+               history={history}
+            />
+         ) : (
+            <div>
+               <h4>{question.subject}</h4>
+               <div>
+                  {Object.values(question.files).map((file, index) => (
+                     <a
+                        href={file.URL}
+                        download={file.name}
+                        key={index}
+                        target="_blank"
+                        rel="noreferrer"
+                     >
+                        {file.name}
+                     </a>
+                  ))}
+               </div>
+               <div>{question.text}</div>
+               <div>
+                  <button>삭제</button>
+                  <button>수정</button>
+               </div>
+               <div>
+                  <form onSubmit={onSubmit}>
+                     <span>댓글</span>
+                     <br></br>
+                     <input
+                        name="newComment"
+                        value={newComment}
+                        onChange={onChange}
+                        maxLength={1000}
+                        style={{ width: 500, height: 80 }}
+                     ></input>
+                     <br />
+                     <input type="submit" value="등록" />
+                  </form>
+               </div>
+               {Object.values(comments).map((comment, index) => (
+                  <div key={index}>
+                     <span>{comment.comment}</span>
+                     <span>{comment.createdAt}</span>
+                  </div>
+               ))}
             </div>
-          ))}
-        </div>
-      )}
-    </>
-  );
+         )}
+      </>
+   );
 }
 export default QuestionDocument;
--- src/components/admin/Conscases.js
+++ src/components/admin/Conscases.js
@@ -1,12 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg.css";
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import { fireStoreService } from "fbase";
 
 function Conscases() {
-  return (
-    <>
-      <Navigation />
-      <div>시공사례입니다.</div>
-    </>
-  );
+   const [Editing, setEditing] = useState(false);
+   const [subject, setSubject] = useState("");
+   const [check, setCheck] = useState(false);
+   const [editorToHtml, setEditorToHTML] = useState(null);
+
+   const onEditingClick = () => {
+      setEditing((prev) => !prev);
+   };
+   const onChange = (event) => {
+      const {
+         target: { name, value },
+      } = event;
+      if (name === "subject") setSubject((prev) => value);
+   };
+   const onCheck = () => {
+      setEditorToHTML(
+         draftToHtml(convertToRaw(editorState.getCurrentContent()))
+      );
+      setCheck(true);
+      console.log(editorToHtml);
+   };
+   const onUnCheck = () => {
+      setCheck(false);
+   };
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      setEditorToHTML(
+         draftToHtml(convertToRaw(editorState.getCurrentContent()))
+      );
+      const conscaseObj = {
+         subject: subject,
+         html: editorToHtml,
+         createdAt: Date.now(),
+      };
+      await fireStoreService.collection("conscase").add(conscaseObj);
+      setEditing((prev) => !prev);
+   };
+   const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+   const onEditorStateChange = (editorState) => {
+      setEditorState((prev) => editorState);
+   };
+
+   return (
+      <>
+         {Editing ? (
+            <>
+               <form onSubmit={onSubmit}>
+                  <span>제목 : </span>
+                  <input
+                     type="text"
+                     name="subject"
+                     value={subject}
+                     onChange={onChange}
+                  />
+                  <Editor
+                     wrapperClassName="wrapper-class"
+                     editorClassName="editor"
+                     toolbarClassName="toolbar-class"
+                     toolbar={{
+                        list: { inDropdown: true },
+                        textAlign: { inDropdown: true },
+                        link: { inDropdown: true },
+                        history: { inDropdown: false },
+                     }}
+                     placeholder="내용을 작성해주세요."
+                     localization={{
+                        locale: "ko",
+                     }}
+                     editorState={editorState}
+                     onEditorStateChange={onEditorStateChange}
+                  />
+                  <input type="submit" value="제출" />
+               </form>
+               <button onClick={onEditingClick}>취소</button>
+               <button onClick={onCheck}>확인</button>
+               {check && (
+                  <>
+                     <div dangerouslySetInnerHTML={{ __html: editorToHtml }} />
+                     <button onClick={onUnCheck}>확인 취소</button>
+                  </>
+               )}
+            </>
+         ) : (
+            <>
+               <Navigation />
+               <div>시공사례입니다.</div>
+               <button onClick={onEditingClick}>작성</button>
+            </>
+         )}
+      </>
+   );
 }
 export default Conscases;
